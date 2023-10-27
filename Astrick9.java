84bc4fa31b2ff4c4341f4cee104079bdf3a9c360
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-27 23:48:16 +0530

    Add files via upload

---
+++ Astrick9.java
@@ -0,0 +1,23 @@
+package Astricks;
+
+public class Astrick9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=4;i++) {
+			for(int j=3;j>=i;j--) {
+				System.out.print(" ");
+				}
+			for(int k=1;k<=i;k++) {
+				System.out.print("*");
+			}
+			for(int y=2;y<=i;y++) {
+			System.out.print("*");
+			
+		}
+			System.out.println();
+		
+	}
+}
+}
