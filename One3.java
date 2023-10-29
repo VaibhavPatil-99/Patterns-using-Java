7265ec4b43b8240c6dbfbb06a469df1d67fa13fe
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-10-29 10:53:35 +0530

    Add files via upload

---
+++ One3.java
@@ -0,0 +1,18 @@
+package Astricks;
+
+public class One3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int count=0;
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+			
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
