28de88b9edf9f18f611139a4343ee2637d7b6753
Vaibhav Patil <110226013+VaibhavPatil-99@users.noreply.github.com>
2023-11-02 00:25:50 +0530

    Add files via upload

---
+++ Abcd3.java
@@ -0,0 +1,18 @@
+package Astricks;
+
+public class Abcd3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char ch='A';
+		for(int i=1;i<=5;i++) {
+			ch = 'A';
+			for(int j=5;j>=i;j--) {
+				System.out.print(ch++);;
+			}
+			System.out.println();
+		}
+	}
+
+}
